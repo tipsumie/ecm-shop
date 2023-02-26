8c67125bf6710a736ac84dd6fe258dc581ef1eea
tipsumie <tipsukon.prm@gmail>
2023-02-26 22:25:49 +0700

    add param for fetching product data by id from api

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -36,7 +36,7 @@ export const fetchProducts = createAsyncThunk(
 
 export const fetchProductById = createAsyncThunk(
   `products/fetchProductById`,
-  async () => {
+  async (id) => {
     const response = await axios.get(`apiUrl/${id}`);
     return response.data;
   }
