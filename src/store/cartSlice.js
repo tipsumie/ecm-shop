fd8626bf94fdc16a098b958d3132dd172af1a345
tipsumie <tipsukon.prm@gmail>
2023-02-27 20:25:05 +0700

    create add to cart action

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,11 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {};
+const initialState = { items: [], total: 0 };
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const isItemInCart = state.carts.find(item => item.id === action.payload.id);
+
+      if(isItemInCart){
+          const tempCart = state.carts.map(item => {
+              if(item.id === action.payload.id){
+                  let tempQty = item.quantity + action.payload.quantity;
+                  let tempTotalPrice = tempQty * item.price;
+
+                  return {
+                      ...item, quantity: tempQty, totalPrice: tempTotalPrice
+                  }
+              } else {
+                  return item;
+              }
+          });
+
+          state.carts = tempCart;
+   
+      } else {
+          state.carts.push(action.payload);
+         
+      }
+  },
+  },
 });
 
+export const {addToCart} = cartSlice.actions;
 export default cartSlice.reducer;
