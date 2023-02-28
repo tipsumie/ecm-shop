f0c17a64f19513c1347e34b58e1f01965772ff93
tipsumie <tipsukon.prm@gmail>
2023-02-28 15:51:06 +0700

    remove .css files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import './App.css';
 
 
 function App() {
